83b17add42afacf9ea5943e764b0a195b69dc8b1
Mehul Sangal <mehulthecoder@gmail.com>
2026-08-13 23:08:50 +0530

    Update b Sum of digits of number problem.c

--- b Sum of digits of number problem.c
+++ b Sum of digits of number problem.c
@@ -1,7 +1,7 @@
 //write a program to find the sum of the digits of a four-digit number(ex. 1234 sum = 10) without using a loop//
 #include <stdio.h>
 int main(){
-    int s,a,b,c,d,e,f,g;
+    int s,a,b,c,d,e,f,g,h;
     printf("User enter the number he wished to take sum of digit:  ");
     scanf("%d", &s);
     a = s/1000;       
@@ -10,7 +10,7 @@ int main(){
     d = ((s%1000)%100)/10;
     e = ((s%1000)%100)%10;
     g = (((s%1000)%100)%10)/1;
-    e = a+b+d+g;
-    printf("The answer is %d", e);
+    h = a+b+d+g;
+    printf("The answer is %d", h);
     return 0;
 }
