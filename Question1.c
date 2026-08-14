59f8964895ea6d8bd2d6569e3a01f15c3a52d508
Mehul Sangal <mehulthecoder@gmail.com>
2026-08-14 11:06:10 +0530

    Add files via upload

---
+++ Question1.c
@@ -0,0 +1,14 @@
+//multiply two number//
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("User enter the value of a\n");
+    scanf("%d", &a);
+    printf("user enter the value of b\n");
+    scanf("%d", &b);
+    printf("The value enter by user is a = %d and b =%d",a,b);
+    c = a*b;
+    printf("The product of a = %d and b = %d is %d", a,b,c);
+    return 0;
+
+}
